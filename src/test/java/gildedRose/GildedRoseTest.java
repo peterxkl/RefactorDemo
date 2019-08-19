5b7bc75c048d8c633d031a4c769733ccc34a32f2
Dillon <18711345569@163.com>
2019-08-19 20:34:49 +0800

    commit test about some condition when name is Backstage

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -42,5 +42,63 @@ public class GildedRoseTest {
         assertEquals(32,items[0].quality);
     }
 
+    @Test
+    public void should_return_19_and_31_when_name_is_Backstage_sellin_is_20_and_quality_is_30(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert",20,30);
+        Item[] items = new Item[]{item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(19,items[0].sellIn);
+        assertEquals(31,items[0].quality);
+    }
 
+    @Test
+    public void should_return_9_and_32_when_name_is_Backstage_sellin_is_10_and_quality_is_30(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert",10,30);
+        Item[] items = new Item[]{item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(9,items[0].sellIn);
+        assertEquals(32,items[0].quality);
+    }
+
+    @Test
+    public void should_return_6_and_32_when_name_is_Backstage_and_sellin_is_7_and_quality_is_30(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert",7,30);
+        Item[] items = new Item[]{item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(6,items[0].sellIn);
+        assertEquals(32,items[0].quality);
+    }
+
+    @Test
+    public void should_return_4_and_33_when_name_is_Backstage_and_sellin_is_5_and_quality_is_30(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert",5,30);
+        Item[] items = new Item[]{item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(4,items[0].sellIn);
+        assertEquals(33,items[0].quality);
+    }
+
+    @Test
+    public void should_return_4_and_0_when_name_is_Backstage_and_sellin_is_0_and_quality_is_30(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert",0,30);
+        Item[] items = new Item[]{item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(-1,items[0].sellIn);
+        assertEquals(0,items[0].quality);
+    }
+
+    @Test
+    public void should_return_negative_7_and_0_when_name_is_Backstage_and_sellin_is_negative_6_and_quality_is_30(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert",-6,30);
+        Item[] items = new Item[]{item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(-7,items[0].sellIn);
+        assertEquals(0,items[0].quality);
+    }
 }
